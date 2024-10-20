4691ffe73d1cf48a717a127ff1c87ef9ff23dc5a
Suresh <sureshabde172@gmail.com>
2024-10-20 05:46:38 -0700

    u

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 
 const App = () => {
   const [data, setData] = useState([]);
-  const [feedback, setFeedback] = useState({}); 
+  const [feedback, setFeedback] = useState({});
 
   useEffect(() => {
     setData(Data);
@@ -14,7 +14,7 @@ const App = () => {
 
   const handleKeyDown = (e, correctAnswer, index) => {
     if (e.key === 'Enter') {
-      const inputValue = e.target.value;
+      const inputValue = e.target.value.trim();
       setFeedback((prev) => ({
         ...prev,
         [index]: inputValue === correctAnswer ? "correct" : "wrong"
@@ -24,37 +24,37 @@ const App = () => {
 
   return (
     <div className='love'>
-      <h2>Find who i am</h2>
-    <div className='hi'>
-      {data.map((item, index) => (
-        <div className='hlo' key={index}>
-          <img src={item.image} alt={`Image of ${item.Hint1}`} className='how' />
-          <ul className='hints'>
-            <li>{item.Hint1}</li>
-            <li>{item.Hint2}</li>
-            <li>{item.Hint3}</li>
-            <li>
-              {feedback[index] === "correct" && (
-                <FontAwesomeIcon icon={faCheckCircle} style={{ color: 'green' }} />
-              )}
-              {feedback[index] === "wrong" && (
-                <FontAwesomeIcon icon={faTimesCircle} style={{ color: 'red' }} />
-              )}
-            </li>
-          </ul>
-          <input
-            type='text'
-            placeholder='Enter here'
-            onKeyDown={(e) => handleKeyDown(e, item.Answer, index)} 
-            className='input'
-          />
-          <br />
+      <h2>Find who I am</h2>
+      <div className='hi'>
+        {data.map((item, index) => (
+          <div className='hlo' key={index}>
+            <img src={item.image} alt={`Image of ${item.Hint1}`} className='how' />
+            <ul className='hints'>
+              <li>{item.Hint1}</li>
+              <li>{item.Hint2}</li>
+              <li>{item.Hint3}</li>
+              <li>
+                {feedback[index] === "correct" && (
+                  <FontAwesomeIcon icon={faCheckCircle} style={{ color: 'green' }} />
+                )}
+                {feedback[index] === "wrong" && (
+                  <FontAwesomeIcon icon={faTimesCircle} style={{ color: 'red' }} />
+                )}
+              </li>
+            </ul>
+            <input
+              type='text'
+              placeholder='Enter here'
+              onKeyDown={(e) => handleKeyDown(e, item.Answer, index)}
+              className='input'
+            />
+            <br />
+          </div>
+        ))}
+        <div>
+          <h2 id='vv'>Thanks for playing</h2>
         </div>
-      ))}
-      <div>
-        <h2 id='vv'>Thanks for play</h2>
       </div>
-    </div>
     </div>
   );
 };
